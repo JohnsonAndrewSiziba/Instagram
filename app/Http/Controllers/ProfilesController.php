8cb82962a76d25e7cefb50fbcc596eee34f3ae9a
Johnson A.Siziba <siziba.uz@gmail.com>
2020-01-17 11:28:36 +0200

    working

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -15,11 +16,14 @@ class ProfilesController extends Controller
     }
 
     public  function  edit(\App\User $user){
+        $this->authorize('update', $user->profile);
         return view('profiles.edit', compact('user'));
     }
 
     public  function  update(\App\User $user)
     {
+        $this->authorize('update', $user->profile);
+
         $data = \request()->validate([
             "title" => "required",
             "description" => "required",
@@ -30,6 +34,13 @@ class ProfilesController extends Controller
         //dd($data);
 
         auth()->user->profile->update($data);
+
+        if (\request('image')) {
+            $imagePath = \request('image')->store('profile', 'public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+            $image->save();
+        }
 
         return redirect("/profile/{$user->id}");
 
